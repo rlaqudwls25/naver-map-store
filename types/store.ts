6edb637185845d47198c4aef945c7a51c72e1123
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-06-03 18:07:30 +0900

    [feat]: json data, 마켓정보 type 추가

--- types/store.ts
+++ types/store.ts
@@ -1,3 +1,23 @@
 type Lat = number
 type Lng = number
 export type Coordinates = [Lat, Lng]
+
+export interface Menu {
+  name: 'string'
+  price: 'string'
+}
+
+export interface StoreInfo {
+  nid: number
+  season: number
+  episode: number
+  characteristic: string
+  name: string
+  coordinates: Coordinates
+  foodKind: string
+  address: string
+  phone: string
+  images: string[]
+  description: string
+  menus: Menu[]
+}
