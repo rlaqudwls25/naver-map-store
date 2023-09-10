71cd30a38ed5bece3de57a54424013965b04a183
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-09-11 01:28:05 +0900

    [fix]: baseURL 수정

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { Axios } from 'axios'
 
 const clientAxios: Axios = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   headers: {
     'Content-Type': 'application/json',
   },
