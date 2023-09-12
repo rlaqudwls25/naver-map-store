2a178a584ebcf9340185d0eb6ebfd19d6861f740
rlaqudwls25 <rlaqudwls157@gmail.com>
2023-09-12 17:39:22 +0900

    [feat]: og:images 추가 하기

--- seo.config.js
+++ seo.config.js
@@ -2,7 +2,12 @@ const SEO_OPTION = {
   titleTemplate: '%s - 매장 서비스',
   openGrapgh: {
     type: 'website',
-    site_name: '네이버 맵 매장',
+    site_name: '매장 서비스 이용하기',
+    images: [
+      {
+        url: 'https://naver-map.vercel.app/public/images/gom.png',
+      },
+    ],
 
     additionalLinkTags: [
       {
